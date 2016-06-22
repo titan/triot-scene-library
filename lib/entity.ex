4a8e92dbaa15186fee08cca468adbf6f6964d6a8
Titan <titan@kachebang.com>
2016-06-22 18:06:26 +0800

    Return x y of devices of scene instead of just did

--- lib/entity.ex
+++ lib/entity.ex
@@ -9,8 +9,8 @@ defmodule Scene.Entity do
   |image|string|场景图 URL|
   |width|boolean|场景图宽度(像素)|
   |height|string|场景图高度(像素)|
-  |devices|[uuid]|设备|
+  |devices|[%{atom => string|int}]|设备列表|
   """
   defstruct sid: nil, name: nil, image: nil, width: 0, height: 0, devices: []
-  @type t :: %Scene.Entity{ sid: String.t, name: String.t, image: String.t, width: non_neg_integer, height: non_neg_integer, devices: [String.t]}
+  @type t :: %Scene.Entity{ sid: String.t, name: String.t, image: String.t, width: non_neg_integer, height: non_neg_integer, devices: [%{atom => String.t | non_neg_integer}] }
 end
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Scene.Library.Mixfile do
 
   def project do
     [app: :scene_library,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
